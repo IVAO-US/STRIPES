bc0b5bf5fd54c925b873c6c079f855426566209a
Wes Sleeman <weston@wsleeman.com>
2025-07-04 21:23:04 -0700

    Add project files.

---
+++ STRIPES/GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using System.Collections.Immutable;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using STRIPES.Client;
+global using STRIPES.DataContracts;
+global using STRIPES.DataContracts.Serialization;
+global using STRIPES.Models;
+global using STRIPES.Services.Endpoints;
+global using Uno.Extensions.Http.Kiota;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
+[assembly: Uno.Extensions.Reactive.Config.BindableGenerationTool(3)]
