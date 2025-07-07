8a8b220b1d85dd73e3814665f59d40f24e82fbce
Wes Sleeman <weston@wsleeman.com>
2025-07-07 10:57:22 -0700

    Added a basic canvas element. We'll see if it works!

---
+++ STRIPES/Pages/ScopeCanvas.skia.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp;
+
+using Windows.Foundation;
+using Uno.WinUI.Graphics2DSK;
+
+namespace STRIPES.Pages;
+
+public class ScopeCanvas : SKCanvasElement
+{
+	protected override void RenderOverride(SKCanvas canvas, Size area)
+	{
+		canvas.Clear(SKColor.Empty);
+		canvas.DrawText("Hello, world!", (float)area.Width / 2, (float)area.Height / 2, SKTextAlign.Center, SKTypeface.Default.ToFont(), new SKPaint() {
+			Color = new(0xFF, 0xFF, 0xFF),
+			IsStroke = false
+		});
+	}
+}
